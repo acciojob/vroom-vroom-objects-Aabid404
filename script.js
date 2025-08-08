87bb2717d4b9402b9360b4b1adeffd21b58ef9cb
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-08-08 14:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getMakeModel=function (){
+	return'${this.make} ${this.model}';
+};
 
-function SportsCar(make, model, topSpeed) {}
+//SportCar Constructor
+
+function SportsCar(make, model, topSpeed) {
+	Car.call (this, make, model);
+	this.topSpeed=topSpeed;
+
+	SportsCar.prototype=object.create(car.prototype);
+	SportsCar.protoptype.constructor=SportsCar;
+
+	SportsCar.prototype.getTopSpeed=function () {
+		return this.topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
